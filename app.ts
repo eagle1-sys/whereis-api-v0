4dca0ceb9de2cba3646c68ac94cdf347a52b94b4
samshdn <samshdn@hotmail.com>
2025-03-28 21:03:46 +0800

    refine: move meta data loading to app

---
+++ app.ts
@@ -0,0 +1,44 @@
+/**
+ * @file app.ts
+ * @description A collection of functions for loading metadata from the file system.
+ * @author samshdn
+ * @date 2025-03-28
+ * @version 0.1.1
+ */
+
+import { load } from "https://deno.land/std/dotenv/mod.ts";
+
+import { loadJSONFromFs } from "./util.ts";
+import { CodeDesc, ErrorRegistry } from "./model.ts";
+
+/**
+ * Loads environment variables from a `.env` file and sets them in `Deno.env`.
+ * @async
+ * @returns {Promise<void>} A promise that resolves when the environment variables are loaded and set.
+ * @throws {Error} If the `.env` file cannot be loaded or parsed.
+ */
+export async function loadEnv(): Promise<void> {
+    const env = await load({ envPath: "./.env" });
+    for (const [key, value] of Object.entries(env)) {
+        // set environment variable to Deno.env
+        Deno.env.set(key, value);
+    }
+}
+
+/**
+ * Loads metadata from the file system, including status codes and error definitions.
+ * Initializes the `CodeDesc` and `ErrorRegistry` classes with the loaded data.
+ * @async
+ * @returns {Promise<void>} A promise that resolves when the metadata is loaded and initialized.
+ * @throws {Error} If the JSON files cannot be loaded or parsed.
+ */
+export async function loadMetaData(): Promise<void> {
+    const status: Record<string, any> = await loadJSONFromFs(
+        "./metadata/codes.json",
+    );
+    CodeDesc.initialize(status);
+    const errors: Record<string, any> = await loadJSONFromFs(
+        "./metadata/errors.json",
+    );
+    ErrorRegistry.initialize(errors);
+}
--- main.ts
+++ main.ts
@@ -5,46 +5,11 @@
  * @author samshdn
  * @version 0.1.1
  */
-import { load } from "https://deno.land/std/dotenv/mod.ts";
 import { logger } from "./logger.ts";
-import { loadJSONFromFs } from "./util.ts";
 import { initializeDbPool } from "./database/dbutil.ts";
-// import { startScheduler } from "./schedule.ts";
 import { Server } from "./server.ts";
 import { syncRoutes } from "./schedule.ts";
-import { CodeDesc, ErrorRegistry } from "./model.ts";
-
-/**
- * Loads environment variables from a `.env` file and sets them in `Deno.env`.
- * @async
- * @returns {Promise<void>} A promise that resolves when the environment variables are loaded and set.
- * @throws {Error} If the `.env` file cannot be loaded or parsed.
- */
-async function loadEnv(): Promise<void> {
-  const env = await load({ envPath: "./.env" });
-  for (const [key, value] of Object.entries(env)) {
-    // set environment variable to Deno.env
-    Deno.env.set(key, value);
-  }
-}
-
-/**
- * Loads metadata from the file system, including status codes and error definitions.
- * Initializes the `CodeDesc` and `ErrorRegistry` classes with the loaded data.
- * @async
- * @returns {Promise<void>} A promise that resolves when the metadata is loaded and initialized.
- * @throws {Error} If the JSON files cannot be loaded or parsed.
- */
-async function loadMetaData(): Promise<void> {
-  const status: Record<string, any> = await loadJSONFromFs(
-    "./metadata/codes.json",
-  );
-  CodeDesc.initialize(status);
-  const errors: Record<string, any> = await loadJSONFromFs(
-    "./metadata/errors.json",
-  );
-  ErrorRegistry.initialize(errors);
-}
+import { loadEnv, loadMetaData } from "./app.ts";
 
 /**
  * Main entry point of the application.
@@ -55,9 +20,9 @@ async function loadMetaData(): Promise<void> {
  * @throws {Error} If any step in the initialization process fails.
  */
 async function main(): Promise<void> {
-  await loadEnv(); // load environment variable first
-  await loadMetaData(); // load file system data
-  initializeDbPool(); // initialize database connection pool
+  await loadEnv();        // load environment variable first
+  await loadMetaData();   // load file system data
+  initializeDbPool();     // initialize database connection pool
 
   /**
    * Starts a scheduler that periodically synchronizes tracking routes.
