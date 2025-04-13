b648a167da79be5a729543b2c8daa3c97e754251
samshdn <samshdn@hotmail.com>
2025-04-13 21:22:45 +0800

    Enhancement: logging directly to the console #35

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM denoland/deno
 WORKDIR /app
 COPY . .
 RUN deno cache main/main.ts
-CMD ["run", "--allow-import","--allow-net", "--allow-env", "--allow-read", "--allow-write","main/main.ts"]
+CMD ["run", "--allow-import","--allow-net", "--allow-env", "--allow-read", "main/main.ts"]
--- tools/logger.ts
+++ tools/logger.ts
@@ -1,13 +1,11 @@
 /**
  * @file logger.ts
  * @description Provides a logger instance for consistent logging across the application
-  */
+ */
 
-import { FileLogger, Log } from "@cross/log";
-
+import { ConsoleLogger, Log, Severity } from "@cross/log";
 export const logger = new Log([
-  new FileLogger({
-    filePath: "./app.log",
-    fileFormat: "txt",
+  new ConsoleLogger({
+    minimumSeverity: Severity.Info,
   }),
 ]);
