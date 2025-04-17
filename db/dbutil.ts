29a790b101dba68a130c46f8f7cbfb9a95e8f00d
samshdn <samshdn@hotmail.com>
2025-04-17 11:17:00 +0800

    Enhancement: Add db connection error handling  #62

--- db/dbutil.ts
+++ db/dbutil.ts
@@ -1,8 +1,9 @@
 /**
  * @file dbutil.ts
  * @description Provides functions to initialize and manage PostgreSQL database connection pool
-  */
-import { Pool } from "https://deno.land/x/postgres/mod.ts";
+ */
+import { Pool, PoolClient } from "https://deno.land/x/postgres/mod.ts";
+import { logger } from "../tools/logger.ts";
 const POOL_CONNECTIONS = 20;
 
 /**
@@ -23,40 +24,42 @@ let dbPool: Pool | null = null;
  * ```
  */
 export function initializeDbPool(): Pool {
-    if (!dbPool) {
-        dbPool = new Pool(
-            {
-                database: Deno.env.get("DATABASE_NAME"),
-                hostname: Deno.env.get("DATABASE_HOST"),
-                password: Deno.env.get("DATABASE_PASSWORD"),
-                port: Deno.env.get("DATABASE_PORT"),
-                user: Deno.env.get("DATABASE_USER"),
-            },
-            POOL_CONNECTIONS,
-        )
-    }
-    return dbPool;
+  if (!dbPool) {
+    dbPool = new Pool(
+      {
+        database: Deno.env.get("DATABASE_NAME"),
+        hostname: Deno.env.get("DATABASE_HOST"),
+        password: Deno.env.get("DATABASE_PASSWORD"),
+        port: Deno.env.get("DATABASE_PORT"),
+        user: Deno.env.get("DATABASE_USER"),
+      },
+      POOL_CONNECTIONS,
+    );
+  }
+  return dbPool;
 }
 
 /**
  * Establishes a connection from the database pool
- *
  * @async
- * @returns {Promise<Client>} A Promise that resolves to a database client connection
+ * @returns {Promise<PoolClient>} A Promise that resolves to a database client connection
  * @throws {Error} If the database pool hasn't been initialized
- *
- * @example
- * ```typescript
- * async function queryDatabase() {
- *   const client = await connect();
- *   // Use client for database operations
- *   await client.end();
- * }
- * ```
  */
-export async function connect() {
-    if (!dbPool) {
-        throw new Error("Database pool not initialized. Call initializeDbPool first.");
-    }
+export async function connect(): Promise<PoolClient> {
+  if (!dbPool) {
+    throw new Error(
+      "Database pool not initialized. Call initializeDbPool first.",
+    );
+  }
+  try {
     return await dbPool.connect();
+  } catch (error) {
+      if(error instanceof Error) {
+          logger.error("Error connecting to the database:", error.message);
+          throw new Error(`Failed to connect to the database: ${error.message}`);
+      } else {
+          logger.error("Unknown error connecting to the database");
+          throw new Error("Failed to connect to the database: Unknown error");
+      }
+  }
 }
